6f5f85a04412b1b01e59fcf1552865a4edb65f76
Christian Rosado <christian.rosado20@upr.edu>
2020-02-28 15:35:17 -0400

    P1 and P2 DONE

---
+++ Solution/P2/score.js
@@ -0,0 +1,41 @@
+var teamABoard = document.getElementById('team-a');
+var teamBBoard = document.getElementById('team-b');
+var scoreATag = document.getElementById('score-a');
+var scoreA = 0;
+var scoreBTag = document.getElementById('score-b');
+var scoreB = 0;
+var maxScoreTag = document.getElementById('max-score');
+var maxScore = maxScoreTag.value;
+var reset = document.getElementById('reset');
+
+
+scoreATag.innerHTML = scoreA;
+scoreBTag.innerHTML = scoreB;
+
+teamABoard.addEventListener('click', function() {
+  scoreA++;
+  scoreATag.innerHTML = scoreA;
+  if(scoreA == maxScoreTag.value && scoreA != 0) {
+    alert('Team A won!');
+    teamABoard.style.backgroundColor = "#00CC6A";
+    teamBBoard.style.backgroundColor = "#FF4343";
+  }
+});
+
+teamBBoard.addEventListener('click', function() {
+  if (scoreB == maxScoreTag.value && scoreB != 0) {
+    alert('Team B won!');
+    teamABoard.style.backgroundColor = "#FF4343";
+    teamBBoard.style.backgroundColor = "#00CC6A";
+  }
+  scoreB++;
+  scoreBTag.innerHTML = scoreB;
+});
+
+reset.addEventListener('click', function() {
+  scoreA = 0;
+  scoreB = 0;
+  maxScoreTag.value = 0;
+  scoreATag.innerHTML = scoreA;
+  scoreBTag.innerHTML = scoreB;
+})
